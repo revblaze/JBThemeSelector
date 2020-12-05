8b3367c98c20f18540cb98e83282e290f9d9f1bc
Justin Bush <revblaze@users.noreply.github.com>
2020-12-05 12:24:30 -0500

    Add Theme, TSWindowController

---
+++ macOS/JBThemeSelector/TSWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  TSWindowController.swift
+//  JBThemeSelector
+//
+//  Created by Justin Bush on 2020-12-05.
+//
+
+import Cocoa
+
+class TSWindowController: NSWindowController, NSToolbarDelegate {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
---
+++ macOS/JBThemeSelector/Theme.swift
@@ -0,0 +1,34 @@
+//
+//  Theme.swift
+//  JBThemeSelector
+//
+//  Created by Justin Bush on 2020-12-05.
+//
+
+import Cocoa
+
+enum Theme {
+    
+    case solid      // Opaque Background
+    case misty      // Half & Half (Middle)
+    case glass      // Translucent Blur
+    
+    /// Determines if the ViewController should have an opaque background or not.
+    var isTransparent: Bool {
+        switch self {
+        case .solid: return false
+        case .misty: return true
+        case .glass: return true
+        }
+    }
+    
+    /// The material type of the NSVisualEffectView.
+    var material: NSVisualEffectView.Material {
+        switch self {
+        case .solid: return .windowBackground
+        case .misty: return .underWindowBackground
+        case .glass: return .fullScreenUI
+        }
+    }
+    
+}
